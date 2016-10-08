401e3cc445edf70513c9c38078616af3c406889d
dotjamie <dotjamie@gmail.com>
2016-10-08 12:42:43 -0700

    basic server

---
+++ app.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+app.use(express.static('node_modules'));
+
+app.listen(5000, function () {
+  console.log('Example app listening on port 5000!');
+});
